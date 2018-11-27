0ac1843c898d1e79f4e24170754621d47bc2d47d
Julian Avila Polanco <julian@julians-mac.local>
2018-11-27 02:14:43 -0800

    Se agregan funcionalidades de periferico bluetooth al CollectionView y también la posibilidad de enviar datos dependiendo de la celda seleccionada.

--- Bluetooth/CollectionViewController.swift
+++ Bluetooth/CollectionViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 private let reuseIdentifier = "Cell"
 
-class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, CBPeripheralManagerDelegate {
 
-   
+    var peripheralManager : CBPeripheralManager?
+    
+    let datosDeAnuncio : [String : Any] = [
+        CBAdvertisementDataLocalNameKey : "Acelerometro iOS",
+        CBAdvertisementDataServiceUUIDsKey : [CBUUID.init(string: "43AB60E3-5BD1-481D-BCE0-3D35543E734F")]
+    ]
+     var seAgregoCaracteristica = false
+    
+    var caracteristica = CBMutableCharacteristic(type: CBUUID.init(string: "BBD46F87-116D-4770-ADC6-3F8243878F57"), properties: .notify, value: nil, permissions: .readable)
+    
+   let nombres = ["Hola","Me","Llamo","Julian"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "El nimo es puto"
-        collectionView.backgroundColor = .white
+        title = "Elegir producto"
+        collectionView.backgroundColor = .gray
         collectionView.showsHorizontalScrollIndicator = false
+         self.peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         
        
         // Uncomment the following line to preserve selection between presentations
@@ -30,16 +42,6 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
         // Do any additional setup after loading the view.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     // MARK: UICollectionViewDataSource
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -50,7 +52,7 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 4
+        return nombres.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -58,53 +60,62 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
         cell.layer.cornerRadius = 20
         cell.clipsToBounds = true
         cell.boton.tag = indexPath.item
-        cell.boton.addTarget(self, action: #selector(hacerAccion(_:)), for: .touchUpInside)
-        
-    
+        cell.boton.setTitle(nombres[indexPath.item], for: .normal)
+        cell.boton.addTarget(self, action: #selector(imprimirBotonPulsado(_:)), for: .touchUpInside)
         return cell
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 1.0, left: 250 / 2, bottom: -50, right: 250 / 2)
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 80
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width - 250, height: 200)
     }
-    // MARK: UICollectionViewDelegate
-
-    @objc func hacerAccion(_ sender: UIButton) {
-        print(sender.tag)
-    }
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        switch peripheral.state {
+        case .poweredOn:
+            //switchAnunciar.isEnabled = true
+            peripheralManager?.startAdvertising(datosDeAnuncio)
+            //collectionView.backgroundColor = .green
+            if seAgregoCaracteristica == false {
+                let servicio = CBMutableService(type: CBUUID.init(string: "43AB60E3-5BD1-481D-BCE0-3D35543E734F"), primary: true)
+                servicio.characteristics = [caracteristica]
+                peripheralManager?.add(servicio)
+                seAgregoCaracteristica = true
+            }
+        case .poweredOff:
+            collectionView.backgroundColor = .gray
+            //switchAnunciar.isEnabled = false
+            peripheralManager?.removeAllServices()
+            peripheralManager?.stopAdvertising()
+        default:
+            peripheralManager?.stopAdvertising()
+        }
     }
-    */
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        collectionView.backgroundColor = .green
+        //labelEstado.textColor = .green
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
+        collectionView.backgroundColor = .gray
+       // labelEstado.text = "Desconectado"
+       // labelEstado.textColor = .gray
+    }
+    
+    @objc func imprimirBotonPulsado(_ sender: UIButton) {
+        print(sender.tag)
+        peripheralManager?.updateValue("\(sender.tag)".data(using: .utf8)!, for: caracteristica, onSubscribedCentrals: nil)
+        
+    }
+   
 
 }
 
@@ -128,6 +139,7 @@ class Celda: UICollectionViewCell {
     }()
     
     
+    
     // Hacemos la inicializacion de la celda utilizando un frame
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -144,8 +156,8 @@ class Celda: UICollectionViewCell {
         self.addSubview(boton)
         boton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         boton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        boton.widthAnchor.constraint(equalTo: self.widthAnchor, constant: -30).isActive = true
-        boton.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -30).isActive = true
+        boton.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        boton.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
         
     }
     
