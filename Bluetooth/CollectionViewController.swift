7c5b308864f10daab3ef9fbac694e86c31033c3e
Julian Avila Polanco <julian@julians-mac.local>
2018-12-02 02:17:40 -0800

    Agregado boton para cancelar el avance de los motores y adaptado para iPad mini

--- Bluetooth/CollectionViewController.swift
+++ Bluetooth/CollectionViewController.swift
@@ -11,8 +11,32 @@ import CoreBluetooth
 
 private let reuseIdentifier = "Cell"
 
-class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, CBPeripheralManagerDelegate {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, CBPeripheralManagerDelegate, SeCancelaElPaso {
+    
+     func cancelarPaso() {
+        print("Cancelando paso")
+        peripheralManager?.updateValue("\(0)".data(using: .utf8)!, for: caracteristica, onSubscribedCentrals: nil)
+    }
+    
 
+    lazy var botonCancelar: UIButton = {
+        let boton = UIButton()
+        boton.setTitle("Cancelar", for: .normal)
+        boton.tintColor = .green
+        boton.titleLabel?.textColor = .black
+        boton.tag = 40
+        boton.addTarget(self, action: #selector(imprimirBotonPulsado(_:)), for: .touchUpInside)
+        return boton
+    }()
+    
+    lazy var switchAnunciar: UISwitch = {
+        let toggle = UISwitch()
+        toggle.isOn = false
+        toggle.addTarget(self, action: #selector(activarAnuncio(_:)), for: .valueChanged)
+       return toggle
+    }()
+    
+    
     var peripheralManager : CBPeripheralManager?
     
     let datosDeAnuncio : [String : Any] = [
@@ -23,7 +47,7 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     
     var caracteristica = CBMutableCharacteristic(type: CBUUID.init(string: "BBD46F87-116D-4770-ADC6-3F8243878F57"), properties: .notify, value: nil, permissions: .readable)
     
-   let nombres = ["Hola","Me","Llamo","Julian"]
+   let nombres = ["Tornillos","Tuercas","Rondanas","Brocas"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +55,7 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
         collectionView.backgroundColor = .gray
         collectionView.showsHorizontalScrollIndicator = false
          self.peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-        
+        self.navigationItem.setLeftBarButton(UIBarButtonItem(customView: switchAnunciar), animated: false)
        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -62,11 +86,12 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
         cell.boton.tag = indexPath.item
         cell.boton.setTitle(nombres[indexPath.item], for: .normal)
         cell.boton.addTarget(self, action: #selector(imprimirBotonPulsado(_:)), for: .touchUpInside)
+        cell.cancelarPasoDelegate = self
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 1.0, left: 250 / 2, bottom: -50, right: 250 / 2)
+        return UIEdgeInsets(top: 1.0, left: 250 / 2, bottom: 50, right: 250 / 2)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -74,14 +99,14 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width - 250, height: 200)
+        return CGSize(width: collectionView.frame.width - 250, height: collectionView.frame.height - 270)
     }
     
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         switch peripheral.state {
         case .poweredOn:
             //switchAnunciar.isEnabled = true
-            peripheralManager?.startAdvertising(datosDeAnuncio)
+            //peripheralManager?.startAdvertising(datosDeAnuncio)
             //collectionView.backgroundColor = .green
             if seAgregoCaracteristica == false {
                 let servicio = CBMutableService(type: CBUUID.init(string: "43AB60E3-5BD1-481D-BCE0-3D35543E734F"), primary: true)
@@ -100,7 +125,7 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        collectionView.backgroundColor = .green
+        collectionView.backgroundColor = .white
         //labelEstado.textColor = .green
     }
     
@@ -112,21 +137,46 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     
     @objc func imprimirBotonPulsado(_ sender: UIButton) {
         print(sender.tag)
-        peripheralManager?.updateValue("\(sender.tag)".data(using: .utf8)!, for: caracteristica, onSubscribedCentrals: nil)
+        peripheralManager?.updateValue("\(sender.tag+1)".data(using: .utf8)!, for: caracteristica, onSubscribedCentrals: nil)
         
     }
+    
+    @objc func activarAnuncio(_ sender: UISwitch) {
+        if sender.isOn {
+            peripheralManager?.startAdvertising(datosDeAnuncio)
+            //print("Prendido...")
+        } else {
+            peripheralManager?.stopAdvertising()
+            //print("Apagado...")
+        }
+    }
+    
    
 
 }
 
+ protocol SeCancelaElPaso {
+    func cancelarPaso()
+}
+
 class Celda: UICollectionViewCell {
-    let label: UILabel = {
-        let label = UILabel()
-        label.text = "Prueba"
-        label.textAlignment = .center
-        label.textColor = .black
-        label.translatesAutoresizingMaskIntoConstraints = false
-       return label
+    
+    var cancelarPasoDelegate: SeCancelaElPaso!
+    
+    let imagen: UIImageView = {
+        let imagen = UIImageView()
+        imagen.contentMode = .scaleAspectFit
+        return imagen
+    }()
+    
+    lazy var botonCancelar: UIButton = {
+        let boton = UIButton()
+        boton.setTitle("Cancelar", for: .normal)
+        boton.setTitleColor(.black, for: .normal)
+        boton.backgroundColor = .orange
+        boton.addTarget(self, action: #selector(llamarFuncionDelProtocolo), for: .touchUpInside)
+        boton.translatesAutoresizingMaskIntoConstraints = false
+       return boton
     }()
     
     lazy var boton: UIButton = {
@@ -138,7 +188,9 @@ class Celda: UICollectionViewCell {
         return boton
     }()
     
-    
+    @objc func llamarFuncionDelProtocolo() {
+        cancelarPasoDelegate.cancelarPaso()
+    }
     
     // Hacemos la inicializacion de la celda utilizando un frame
     override init(frame: CGRect) {
@@ -153,12 +205,17 @@ class Celda: UICollectionViewCell {
 
     func setupUI(){
         self.backgroundColor = .red
+        
         self.addSubview(boton)
+        self.addSubview(botonCancelar)
         boton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         boton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         boton.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
         boton.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
-        
+        botonCancelar.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        botonCancelar.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        botonCancelar.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        botonCancelar.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
     
     
